6f9a48c7f4f8d85d47d9628036325ea3ac1941fc
Zoe27 <husiyu1995@gmail.com>
2026-06-09 07:18:09 +0200

    Show post update dates

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -5,10 +5,12 @@
 Jekyll::Hooks.register :posts, :post_init do |post|
 
   commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  lastmod_date = post.date
 
   if commit_num.to_i > 1
     lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
   end
+
+  post.data['last_modified_at'] = lastmod_date
 
 end
